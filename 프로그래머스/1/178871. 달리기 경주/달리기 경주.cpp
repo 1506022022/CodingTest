63b5bbc08f65d722ab11887c3f2c58f15b2dbf7c
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-04-03 11:51:12 +0900

    Update 달리기 경주.cpp

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -5,29 +5,28 @@
 using namespace std;
 
 vector<string> solution(vector<string> players, vector<string> callings) {
-    // 1. 입력
-    vector<string> answer;
-    for (size_t i = 0; i < players.size(); i++)
-    {
-        answer.push_back(players[i]);
-    }
-    unordered_map<string, int> ranking;
-    for (int i = 0; i < players.size(); i++)
-    {
-        ranking.insert(make_pair(answer[i], i));
-    }
+	// 1. 입력
+	vector<string> answer;
+	for (size_t i = 0; i < players.size(); i++)
+	{
+		answer.push_back(players[i]);
+	}
+	unordered_map<string, int> ranking;
+	for (int i = 0; i < players.size(); i++)
+	{
+		ranking.insert(make_pair(answer[i], i));
+	}
 
-    // 2. 호명
-    for (auto name : callings)
-    {
-        size_t index = ranking[name];
-        string swap = answer[index-1];
-        answer[index - 1] = name;
-        ranking[name] = index - 1;
-        answer[index] = swap;
-        ranking[swap] = index;
-    }
+	// 2. 호명
+	for (auto name : callings)
+	{
+		size_t index = ranking[name];
+		string swapName = answer[index - 1];
+		swap(answer[index], answer[index - 1]);
+		ranking[name] = index - 1;
+		ranking[swapName] = index;
+	}
 
-    // 3. 출력
-    return answer;
+	// 3. 출력
+	return answer;
 }
