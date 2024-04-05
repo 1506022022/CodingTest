3b4888feee77aff6368921faa5731fda059e0f4d
1506022022 <88864717+1506022022@users.noreply.github.com>
2024-04-05 21:21:08 +0900

    Update 공원 산책.cpp

--- 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -18,22 +18,24 @@ struct Move
 	}
 };
 
+const unordered_map<string, Move> Direction =
+{
+	{ "E", Move(1, 0,1)  },
+	{ "W", Move(-1, 0,1) },
+	{ "S", Move(0, 1,1)  },
+	{ "N", Move(0, -1,1) }
+};
+
 vector<int> solution(vector<string> park, vector<string> routes)
 {
-	const unordered_map<string, Move> Directions =
-	{
-		{ "E", Move(1, 0,1)  },
-		{ "W", Move(-1, 0,1) },
-		{ "S", Move(0, 1,1)  },
-		{ "N", Move(0, -1,1) }
-	};
-	vector<int> answer(2);
+	// 1. 입력
+	vector<int> answer(2,0);
 
 	// 1-1. 공원 크기 구하기
 	int parkHeight = park.size();
 	int parkWidth = park[0].size();
 
-	// 1-1. 시작 지점 찾기
+	// 1-2. 시작 지점 찾기
 	for (int y = 0; y < parkHeight; y++)
 	{
 		for (int x = 0; x < parkWidth; x++)
@@ -46,12 +48,12 @@ vector<int> solution(vector<string> park, vector<string> routes)
 		}
 	}
 
-	// 1-2. 명령 읽어들이기
+	// 1-3. 명령 읽어들이기
 	vector<Move> moves;
 	for (string route : routes)
 	{
 		string direction = route.substr(0, 1);
-		Move move = Directions.at(direction);
+		Move move = Direction.at(direction);
 
 		string distance = route.substr(2, route.size() - 2);
 		move.Distance = stoi(distance);
@@ -66,7 +68,7 @@ vector<int> solution(vector<string> park, vector<string> routes)
 		int x;
 		int y;
 		y = answer[0] + move.DY * move.Distance;
-		x = answer[1] + move.DX *move.Distance;
+		x = answer[1] + move.DX * move.Distance;
 		if (x < 0 || parkWidth <= x ||
 			y < 0 || parkHeight <= y)
 		{
@@ -76,24 +78,24 @@ vector<int> solution(vector<string> park, vector<string> routes)
 		// 2-2. 장애물이 있다.
 		int dx = 0;
 		int dy = 0;
-        bool bObstruction = false;
+		bool bObstruction = false;
 		for (int d = 1; d <= move.Distance; d++)
 		{
 			dy = move.DY * d;
 			dx = move.DX * d;
 			if (park[answer[0] + dy][answer[1] + dx] == 'X')
 			{
-                bObstruction=true;
+				bObstruction = true;
 			}
 		}
-        if(bObstruction)
-        {
-            continue;
-        }
-        
-	// 2-3. 여기까지 왔다면 명령을 수행한다.
-	answer[0] += move.DY * move.Distance;
-	answer[1] += move.DX * move.Distance;
+		if (bObstruction)
+		{
+			continue;
+		}
+
+		// 2-3. 여기까지 왔다면 명령을 수행한다.
+		answer[0] += move.DY * move.Distance;
+		answer[1] += move.DX * move.Distance;
 	}
 
 	// 3. 결과 출력.
